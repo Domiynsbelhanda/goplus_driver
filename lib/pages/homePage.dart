2c0d0de2391a6892277163db952966939cefa919
Dominique Youness <youness.dominique@gmail.com>
2022-09-29 04:17:58 +0200

    [FEAT] Read realtime position

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:goplus_driver/widget/notification_dialog.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -9,11 +12,72 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePage extends State<HomePage>{
+
+  LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
+  GoogleMapController? _controller;
+  Location _location = Location();
+  Set<Marker> markers = Set();
+
+  void _onMapCreated(GoogleMapController _cntlr)
+  {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) async {
+      // notification_dialog(
+      //     context,
+      //     'Long : ${l.longitude}, Lat : ${l.latitude}',
+      //     Icons.confirmation_num_sharp,
+      //     Colors.red,
+      //     {'label': 'OK', 'onTap': (){}},
+      //     20,
+      //     true
+      // );
+      BitmapDescriptor markerbitmap = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(),
+        "assets/images/car_android.png",
+      );
+
+      setState(() {
+        markers.add(
+            Marker( //add start location marker
+              markerId: MarkerId('Ma Position'),
+              position: LatLng(l.latitude!, l.longitude!), //position of marker
+              infoWindow: InfoWindow( //popup info
+                title: 'Ma Position',
+                snippet: 'Moi',
+              ),
+              icon: markerbitmap, //Icon for Marker
+            )
+        );
+      });
+
+      _controller!.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 15),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Text('Belhanda'),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(target: _initialcameraposition),
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              markers: markers,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
