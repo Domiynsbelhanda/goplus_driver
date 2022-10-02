6646546c69a3e09412a5f98e2f2984d5d6c942ef
Dominique Youness <youness.dominique@gmail.com>
2022-10-02 03:59:50 +0200

    [FIX] Read courses informations

--- lib/widget/notification_dialog.dart
+++ lib/widget/notification_dialog.dart
@@ -6,76 +6,71 @@ notification_dialog(
     String text,
     IconData? icons,
     Color? color,
-    var button,
     double? fontSize,
     bool? barriere) {
 
-  // set up the button
-  Widget okButton = TextButton(
-    child: Container(
-      padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: AppColors.primaryColor,
-          borderRadius: BorderRadius.circular(8.0)
-        ),
-        child: Text(
-            '${button['label']}',
-          style: TextStyle(
-            color: Colors.black
-          ),
-        )
-    ),
-    onPressed: button['onTap'],
-  );
-
   double width = MediaQuery.of(context).size.width;
-
-  // set up the AlertDialog
-  Dialog alert = Dialog(
-    shape: RoundedRectangleBorder(
-        borderRadius:
-        BorderRadius.circular(20.0)),
-    child: SizedBox(
-      width: width / 1,
-      height: width / 1.1,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children : [
-            Icon(
-              icons,
-              color: color,
-              size: width / 5,
-            ),
-
-            SizedBox(height: 16.0),
-
-            Container(
-              width : width / 1.5,
-              child: Text(
-                '${text}',
-                style: TextStyle(
-                  fontSize: fontSize,
-                  color: Colors.black,
-                )
-              ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            okButton
-          ]
-        )
-      ),
-    ),
-  );
 
   // show the dialog
   showDialog(
     context: context,
     barrierDismissible: barriere!,
-    builder: (BuildContext context) {
-      return alert;
+    builder: (BuildContext contexts) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+            borderRadius:
+            BorderRadius.circular(20.0)),
+        child: SizedBox(
+          width: width / 1,
+          height: width / 1.1,
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                  children : [
+                    Icon(
+                      icons,
+                      color: color,
+                      size: width / 5,
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    Container(
+                      width : width / 1.5,
+                      child: Text(
+                          '${text}',
+                          style: TextStyle(
+                            fontSize: fontSize,
+                            color: Colors.black,
+                          )
+                      ),
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    TextButton(
+                      child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                              color: AppColors.primaryColor,
+                              borderRadius: BorderRadius.circular(8.0)
+                          ),
+                          child: Text(
+                            'FERMER',
+                            style: TextStyle(
+                                color: Colors.black
+                            ),
+                          )
+                      ),
+                      onPressed: (){
+                        Navigator.pop(contexts);
+                      },
+                    )
+                  ]
+              )
+          ),
+        ),
+      );
     },
   );
 }
--- lib/widget/progresso_dialog.dart
+++ lib/widget/progresso_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
+import 'package:goplus_driver/widget/notification_dialog.dart';
 
 import '../utils/app_colors.dart';
 
@@ -15,112 +16,130 @@ progresso_dialog(
   // show the dialog
   showDialog(
     context: contexts,
-    barrierDismissible: true,
+    barrierDismissible: false,
     builder: (BuildContext context) {
       return Dialog(
         shape: RoundedRectangleBorder(
             borderRadius:
             BorderRadius.circular(20.0)),
-        child: SizedBox(
-          width: width / 1,
-          height: width /1.3,
-          child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                  children : [
-                    Text(
-                        'En attente de la réponse',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
+        child: FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance.collection('drivers')
+              .doc(text).collection('courses').doc('courses').get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            
+            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
 
-                    TimerCountdown(
-                      secondsDescription: 'Secondes',
-                      timeTextStyle: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold
-                      ),
-                      format: CountDownTimerFormat.secondsOnly,
-                      endTime: DateTime.now().add(
-                        Duration(
-                          seconds: 40,
-                        ),
-                      ),
-                      onEnd: () {
-                        FirebaseFirestore.instance.collection('drivers').doc(text).update({
-                          'online': true,
-                          'ride': false,
-                          'ride_view': false
-                        });
-                        FirebaseFirestore.instance.collection('drivers').doc(text).collection('courses')
-                            .doc('courses').delete();
-                        Navigator.pop(context);
-                      },
-                    ),
-
-                    Row(
-                      children: [
-                        TextButton(
-                          child: Container(
-                              padding: const EdgeInsets.all(16.0),
-                              decoration: BoxDecoration(
-                                  color: AppColors.primaryColor,
-                                  borderRadius: BorderRadius.circular(8.0)
-                              ),
-                              child: Text(
-                                'REFUSER',
-                                style: TextStyle(
-                                    color: Colors.black
-                                ),
-                              )
+            return SizedBox(
+              width: width / 1,
+              height: width /1.3,
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children : [
+                        Text(
+                          'En attente de la réponse',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold
                           ),
-                          onPressed: (){
-                            FirebaseFirestore.instance.collection('drivers').doc(text).update({
-                              'online': true,
-                              'ride': false
-                            });
-                            FirebaseFirestore.instance.collection('drivers').doc(text).collection('courses')
-                            .doc('courses').delete();
-                            Navigator.pop(context);
-                          },
                         ),
 
-                        const SizedBox(width: 8.0),
-
-                        TextButton(
-                          child: Container(
-                              padding: const EdgeInsets.all(16.0),
-                              decoration: BoxDecoration(
-                                  color: AppColors.primaryColor,
-                                  borderRadius: BorderRadius.circular(8.0)
-                              ),
-                              child: Text(
-                                'REFUSER',
-                                style: TextStyle(
-                                    color: Colors.black
-                                ),
-                              )
+                        TimerCountdown(
+                          secondsDescription: 'Secondes',
+                          minutesDescription: 'Minutes',
+                          timeTextStyle: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold
                           ),
-                          onPressed: (){
+                          format: CountDownTimerFormat.minutesSeconds,
+                          endTime: DateTime.now().add(
+                            Duration(
+                              minutes: 1,
+                              seconds: 40,
+                            ),
+                          ),
+                          onEnd: () {
                             FirebaseFirestore.instance.collection('drivers').doc(text).update({
                               'online': true,
                               'ride': false,
                               'ride_view': false
                             });
                             FirebaseFirestore.instance.collection('drivers').doc(text).collection('courses')
-                                .doc('courses').delete();
+                                .doc('courses')
+                                .update({
+                              'status': 'cancel',
+                            });
                             Navigator.pop(context);
                           },
                         ),
-                      ],
-                    )
-                  ]
-              )
-          ),
-        ),
+
+                        Row(
+                          children: [
+                            TextButton(
+                              child: Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  decoration: BoxDecoration(
+                                      color: AppColors.primaryColor,
+                                      borderRadius: BorderRadius.circular(8.0)
+                                  ),
+                                  child: Text(
+                                    'VOIR',
+                                    style: TextStyle(
+                                        color: Colors.black
+                                    ),
+                                  )
+                              ),
+                              onPressed: (){
+                                FirebaseFirestore.instance.collection('drivers').doc(text).update({
+                                  'online': true,
+                                  'ride': false
+                                });
+                                FirebaseFirestore.instance.collection('drivers').doc(text).collection('courses')
+                                    .doc('courses').delete();
+                                Navigator.pop(context);
+                              },
+                            ),
+
+                            const SizedBox(width: 4.0),
+
+                            TextButton(
+                              child: Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  decoration: BoxDecoration(
+                                      color: AppColors.primaryColor,
+                                      borderRadius: BorderRadius.circular(8.0)
+                                  ),
+                                  child: Text(
+                                    'REFUSER',
+                                    style: TextStyle(
+                                        color: Colors.black
+                                    ),
+                                  )
+                              ),
+                              onPressed: (){
+                                FirebaseFirestore.instance.collection('drivers').doc(text).update({
+                                  'online': true,
+                                  'ride': false,
+                                  'ride_view': false
+                                });
+                                FirebaseFirestore.instance.collection('drivers').doc(text).collection('courses')
+                                    .doc('courses')
+                                    .update({
+                                  'status': 'cancel',
+                                });
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        )
+                      ]
+                  )
+              ),
+            );
+          },
+        )
       );
     },
   );
