dc0bb46d5fe3ea4d1460549fcc2d3a104003f14b
Dominique Youness <youness.dominique@gmail.com>
2022-10-06 09:48:00 +0200

    [FIX] Null position

--- lib/screens/checkPage.dart
+++ lib/screens/checkPage.dart
@@ -32,6 +32,7 @@ class CheckPage extends StatelessWidget{
           return GoogleMapsPolylines(
             destination: LatLng(data['destination_latitude'], data['destination_longitude']),
             origine: LatLng(data['depart_latitude'], data['depart_longitude']),
+            position: LatLng(data['depart_latitude'], data['depart_longitude']),
             id: cle!,
             phone: data['user_id'],
             key: _mapKey,
