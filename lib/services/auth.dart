c9b39668a371a862c047c81f6b6a2283dc38198d
Youness Dominique <youness.dominique@gmail.com>
2022-11-08 15:04:20 +0200

    [FIX] Auth provider

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,24 +2,22 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart' as Dio;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../utils/global_variables.dart';
 import 'dio.dart';
 
 
 class Auth extends ChangeNotifier{
 
-  final storage = new FlutterSecureStorage();
-
-  Future<Map<String, dynamic>> login ({required Map creds, required BuildContext context}) async {
-
+  Future<Map<String, dynamic>> request ({required Map<String, dynamic> data}) async {
     try {
-      Dio.Response response = await dio()!.post('/v1/', data: creds);
+      Dio.Response response = await dio()!.post('/v1/', data: jsonEncode(data));
       Map<String, dynamic> res = jsonDecode(response.data);
       if(response.statusCode == 200){
         return res;
       } else {
         return {
-          'code': "NULL"
+          'code': "NULL",
+          'error': response.statusCode
         };
       }
     } catch (e){
@@ -30,96 +28,6 @@ class Auth extends ChangeNotifier{
     }
   }
 
-  Future<Map<String, dynamic>> register ({required Map<String, dynamic> cred, required BuildContext context}) async {
-
-    try {
-      Dio.Response response = await dio()!.post('/v1/', data: cred);
-      Map<String, dynamic> res = jsonDecode(response.data);
-      if(response.statusCode == 200){
-        return res;
-      } else {
-        return {
-          'code': "NULL"
-        };
-      }
-    } catch (e){
-      return {
-        'code': "ERROR",
-        'error': e
-      };
-    }
-  }
-
-  Future<String> sendOtp(BuildContext context, String phone) async {
-    try{
-      var data = {
-        "key": "otp",
-        "phone": phone
-      };
-      Dio.Response response = await dio()!.post('/v1/', data: jsonEncode(data));
-      Map<String, dynamic> datas = jsonDecode(response.data);
-      notifyListeners();
-      return datas['code'];
-    } catch(e){
-      return "KO";
-    }
-  }
-
-  Future<Map<String, dynamic>> checkOtp(BuildContext context, var data) async {
-    try{
-      Dio.Response response = await dio()!.post('/v1/', data: jsonEncode(data));
-      Map<String, dynamic> datas = jsonDecode(response.data);
-      if(datas['code'] == 'OK'){
-        this.storage.write(key: 'sid', value: datas['sid']);
-        storeToken(token: data['phone']);
-      }
-      notifyListeners();
-      return datas;
-    } catch(e){
-      return {
-        'code': "KO"
-      };
-    }
-  }
-
-  Future<Map<String, dynamic>> checkSID(BuildContext context, var data) async {
-    try{
-      Dio.Response response = await dio()!.post('/v1/', data: jsonEncode(data));
-      Map<String, dynamic> datas = jsonDecode(response.data);
-      notifyListeners();
-      return datas;
-    } catch(e){
-      return {
-        'code': "KO"
-      };
-    }
-  }
-
-  Future<Map<String, dynamic>>
-  storeCourse({required Map<String, dynamic> data, required BuildContext context}) async{
-    try{
-      Dio.Response response = await dio()!.post('/v1/', data: jsonEncode(data));
-      Map<String, dynamic> datas = jsonDecode(response.data);
-
-      var validation = {
-        "key": "ride",
-        "action": "update",
-        "driversid": data['sid_driver'],
-        "rideref": datas['rideref']
-      };
-      await dio()!.post('/v1/', data: jsonEncode(validation));
-
-      notifyListeners();
-      return datas;
-    } catch(e){
-      return {'code': 'KO'};
-    }
-  }
-
-  void storeToken({required String token}) async{
-    this.storage.write(key: 'token', value: token);
-    notifyListeners();
-  }
 
   Future<String?> getToken() async{
     return await storage.read(key: 'token');
