cf4de98246c3705d4bb042717a94506d65a1d728
Youness Dominique <youness.dominique@gmail.com>
2022-11-25 15:35:49 +0200

    [FEAT] Add checking sid information

--- lib/screens/checkPage.dart
+++ lib/screens/checkPage.dart
@@ -8,33 +8,57 @@ import 'enter_phone_number_screen.dart';
 
 class CheckPage extends StatelessWidget{
 
-  String? cle;
-  final Key _mapKey = UniqueKey();
+  CheckPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
           future: Provider.of<Auth>(context, listen: false).getToken(),
           builder: (context, tokens) {
-            if(!tokens.hasData){
-              return PhoneNumberScreen();
+            if(tokens.hasData){
+              return FutureBuilder(
+                future: Provider.of<Auth>(context, listen: false).getSid(),
+                builder: (context, sidSnap){
+                  if(sidSnap.hasData){
+                    driver_sid = sidSnap.data.toString();
+                    return FutureBuilder(
+                      future: Provider.of<Auth>(context, listen: false).request(data: {
+                        'key': 'check_user',
+                        'action': 'sid',
+                        'sid': sidSnap.data.toString(),
+                        'level': '4'
+                      }),
+                      builder: (context, checkSidSnap) {
+                        if(checkSidSnap.hasData){
+                          Map datas = checkSidSnap.data as Map;
+                          if(datas['status'] == 'NOK'){
+                            return PhoneNumberScreen();
+                          }
+                          return StreamBuilder<DocumentSnapshot>(
+                            stream: FirebaseFirestore.instance.collection('drivers')
+                                .doc(tokens.data.toString()).snapshots(),
+                            builder: (context, snapshot){
+                              if(!snapshot.hasData){
+                                return const Center(child: Text('En cours de chargement ...'));
+                              }
+                              return Text('${tokens.data}');
+                              Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+                              driver_token = tokens.data.toString();
+                              driver_data = data;
+                              return HomePage();
+                            },
+                          );
+                        }
+                        return PhoneNumberScreen();
+                      }
+                    );
+                  }
+
+                  return PhoneNumberScreen();
+                },
+              );
             }
-            Provider.of<Auth>(context, listen:false).getSid().then((values){
-              driver_sid = values;
-            });
-            return StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance.collection('drivers')
-                  .doc(tokens.data.toString()).snapshots(),
-              builder: (context, snapshot){
-                if(!snapshot.hasData){
-                  return const Center(child: Text('En cours de chargement ...'));
-                }
-                Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-                driver_token = tokens.data.toString();
-                driver_data = data;
-                return HomePage();
-              },
-            );
+            return PhoneNumberScreen();
           }
         );
   }
